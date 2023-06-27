c7735dfc81dd2d8bc35cd30a2c56696e0e22cdef
josephyu12 <82769846+josephyu12@users.noreply.github.com>
2023-06-27 15:58:29 -0400

    Initial Commit

---
+++ RecipeRecommender/RecipeRecommenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeRecommenderApp.swift
+//  RecipeRecommender
+//
+//  Created by Joseph Yu on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeRecommenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
