11bbcb9ad9bff358e589f4af06b024543043c626
josephyu12 <82769846+josephyu12@users.noreply.github.com>
2023-09-26 00:09:53 -0400

    initial work on pulling from fridge

---
+++ RecipeRecommender/Pages/ScanAddView.swift
@@ -0,0 +1,18 @@
+//
+//  ScanAddView.swift
+//  RecipeRecommender
+//
+//  Created by Joseph Yu on 9/25/23.
+//
+
+import SwiftUI
+
+struct ScanAddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanAddView()
+}
